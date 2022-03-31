137cd2cf5f081ccfc2a4b60385b3abc19f1d563d
quy.duongminh <quy.duongminh@drcomgroup.com>
2022-03-31 22:44:16 +0700

    Boilerplate

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import '../scss/app.scss';
+import 'bootstrap/dist/css/bootstrap.css';
+
+/* Your JS Code goes here */
+
+/* Import dependence */
+import 'bootstrap/dist/js/bootstrap.js';
+
+/* Main JS */
+import './script.js';
---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+// 'Webpack Boilerplate v5.12.1 - SASS/PostCSS, ES6/7, browser sync, source code listing and more.'
+$(document).ready(function(){
+    $('.carousel').carousel({
+        interval: 1000,
+        ride: 'carousel'
+    })
+})
